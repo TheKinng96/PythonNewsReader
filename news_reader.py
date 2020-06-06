9b977aefa493e59f09e1f0fcd29614473e00127f
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-06 10:31:06 +0900

    done the basic get requests

---
+++ news_reader.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+print(create_custom_hn(links, subtext))
